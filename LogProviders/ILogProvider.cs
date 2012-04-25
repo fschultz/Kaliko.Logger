3a2ea8216c2f3291d56a65c74dabe17ae7cf5728
Fredrik Schultz <fschultz@users.noreply.github.com>
2012-04-25 19:50:32 +0000

    Structure applied to the TFS.
    git-svn-id: https://dotnetlogger.svn.codeplex.com/svn/Main/Source/Kaliko.Logger@13598 4a90e977-6436-4932-9605-20e10c2b6fb3

---
+++ LogProviders/ILogProvider.cs
@@ -0,0 +1,24 @@
+﻿#region License and copyright notice
+/* 
+ * Kaliko Logger
+ * 
+ * Copyright (c) 2011 Fredrik Schultz
+ * 
+ * This source is subject to the Microsoft Public License.
+ * See http://www.microsoft.com/opensource/licenses.mspx#Ms-PL.
+ * All other rights reserved.
+ * 
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+ * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ */
+#endregion
+
+namespace Kaliko.LogProviders {
+
+    internal interface ILogProvider {
+        Logger.Severity Treshold { get; }
+
+        void Write(LogItem item);
+    }
+}
